b80e51f12522c83fd06d958c24dd792ad82695dd
jesus <jesuscordovasalcido@gmail.com>
2022-02-21 10:25:28 -0600

    Added the necessary fonts

--- bankodemia/Modules/Welcome/Controller/WelcomeViewController.swift
+++ bankodemia/Modules/Welcome/Controller/WelcomeViewController.swift
@@ -4,8 +4,15 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    var label: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        label = UILabel(frame: CGRect(x: 30, y: 50, width: 1000, height: 599))
+        label?.text = "!Adios a las complicaciones!"
+        label?.font = UIFont(name: "Poppins-SemiBold", size: 20)
+        view.addSubview(label!)
     }
     
 
