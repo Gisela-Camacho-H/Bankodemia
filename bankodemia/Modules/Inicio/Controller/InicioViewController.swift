6e51dbe33874beb5d9f7437847fb9b6be4aaf184
Itzibam <iitziiba3@gmail.com>
2022-03-12 12:12:14 -0600

    Tarjeta and Home View Controllers had been added

--- bankodemia/Modules/Inicio/Controller/InicioViewController.swift
+++ bankodemia/Modules/Inicio/Controller/InicioViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class InicioViewController: UIViewController {
+    
+    var dineroLabel: UILabel = UILabel()
+    var cantidadLabel: UILabel = UILabel()
+    var enviarButton: UIButton = UIButton()
+    var recibirButton: UIButton = UIButton()
+    var fechaLabel: UILabel = UILabel()
+    var backgroundLabel: UILabel = UILabel()
+    var fondoLabel: UILabel = UILabel()
+    var horaLabel: UILabel = UILabel()
+    var movimiento1Label: UILabel = UILabel()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +29,141 @@ class InicioViewController: UIViewController {
     }
     
     func initUI(){
-        view.backgroundColor = .cyan
+        view.backgroundColor = .white
+        
+        
+        //MARK: Primera vista
+        
+        
+
+        dineroLabel.frame = CGRect(x: 0, y: 0, width: 144, height: 21)
+        dineroLabel.backgroundColor = .white
+        dineroLabel.textColor = UIColor(red: 0.384, green: 0.384, blue: 0.384, alpha: 1)
+        dineroLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        
+        dineroLabel.textAlignment = .center
+        dineroLabel.attributedText = NSMutableAttributedString(string: "DINERO DISPONIBLE", attributes: [NSAttributedString.Key.kern: 0.7])
+
+        
+
+        view.addSubview(dineroLabel)
+        dineroLabel.translatesAutoresizingMaskIntoConstraints = false
+        dineroLabel.widthAnchor.constraint(equalToConstant: 144).isActive = true
+        dineroLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        dineroLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22).isActive = true
+        dineroLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 109).isActive = true
+        
+        
+        
+        
+        
+
+        cantidadLabel.frame = CGRect(x: 0, y: 0, width: 85, height: 30)
+        cantidadLabel.backgroundColor = .white
+        cantidadLabel.textColor = UIColor(red: 0.087, green: 0.087, blue: 0.087, alpha: 1)
+        cantidadLabel.font = UIFont(name: "Poppins-SemiBold", size: 20)
+        cantidadLabel.text = "$160.00"
+
+
+        
+
+        view.addSubview(cantidadLabel)
+        cantidadLabel.translatesAutoresizingMaskIntoConstraints = false
+        cantidadLabel.widthAnchor.constraint(equalToConstant: 85).isActive = true
+        cantidadLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        cantidadLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 147).isActive = true
+        cantidadLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 152).isActive = true
+        
+        
+        //MARK: Botones
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        fechaLabel.frame = CGRect(x: 0, y: 0, width: 111, height: 21)
+        fechaLabel.backgroundColor = UIColor(red: 245, green: 245, blue: 245, alpha: 1)
+        fechaLabel.textColor = UIColor(red: 0.384, green: 0.384, blue: 0.384, alpha: 1)
+        fechaLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        fechaLabel.textAlignment = .center
+        fechaLabel.attributedText = NSMutableAttributedString(string: "25 ENERO 2022", attributes: [NSAttributedString.Key.kern: 0.7])
+
+// checa el fondo de esta label
+        
+        view.addSubview(fechaLabel)
+        fechaLabel.translatesAutoresizingMaskIntoConstraints = false
+        fechaLabel.widthAnchor.constraint(equalToConstant: 111).isActive = true
+        fechaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        fechaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22).isActive = true
+        fechaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 272).isActive = true
+        
+        
+        
+        
+        
+        
+      
+
+        fondoLabel.frame = CGRect(x: 0, y: 0, width: 135, height: 21)
+        fondoLabel.backgroundColor = .white
+        fondoLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        fondoLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        fondoLabel.text = "Fondeo Bienvenida"
+
+
+        view.addSubview(fondoLabel)
+        fondoLabel.translatesAutoresizingMaskIntoConstraints = false
+        fondoLabel.widthAnchor.constraint(equalToConstant: 135).isActive = true
+        fondoLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        fondoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22).isActive = true
+        fondoLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 310).isActive = true
+        
+        
+        
+        
+
+        horaLabel.frame = CGRect(x: 0, y: 0, width: 58, height: 21)
+        horaLabel.backgroundColor = .white
+        horaLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        horaLabel.font = UIFont(name: "Poppins-Regular", size: 14)
+        horaLabel.text = "7:18 p.m."
+
+
+       
+
+        view.addSubview(horaLabel)
+        horaLabel.translatesAutoresizingMaskIntoConstraints = false
+        horaLabel.widthAnchor.constraint(equalToConstant: 58).isActive = true
+        horaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        horaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22).isActive = true
+        horaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 326).isActive = true
+        
+        
+        
+        
+        movimiento1Label.frame = CGRect(x: 0, y: 0, width: 67, height: 21)
+        movimiento1Label.backgroundColor = .white
+        movimiento1Label.textColor = UIColor(red: 0, green: 0.8, blue: 0.8, alpha: 1)
+        movimiento1Label.font = UIFont(name: "Poppins-Medium", size: 14)
+        movimiento1Label.attributedText = NSMutableAttributedString(string: "+ $50.00", attributes: [NSAttributedString.Key.kern: 0.7])
+
+
+
+        view.addSubview(movimiento1Label)
+        movimiento1Label.translatesAutoresizingMaskIntoConstraints = false
+        movimiento1Label.widthAnchor.constraint(equalToConstant: 67).isActive = true
+        movimiento1Label.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        movimiento1Label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 274).isActive = true
+        movimiento1Label.topAnchor.constraint(equalTo: view.topAnchor, constant: 310).isActive = true
+        
+        
+        
     }
 
 }
--- bankodemia/Modules/Tarjeta/Controller/TarjetaViewController.swift
+++ bankodemia/Modules/Tarjeta/Controller/TarjetaViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class TarjetaViewController: UIViewController {
+    
+    var tarjetaLabel: UILabel = UILabel()
+    var tarjetaImage: UIImage = UIImage()
+//    necesito declarar mas cosas
+    var noTarjetaLabel: UILabel = UILabel()
+    var noCuentaLabel: UILabel = UILabel()
+    var vigenciaLabel: UILabel = UILabel()
+    var fechaLabel: UILabel = UILabel()
+    var cvvLabel: UILabel = UILabel()
+    var cvvNumeroLabel: UILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +25,154 @@ class TarjetaViewController: UIViewController {
     }
 
     func initUI() {
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        
+  
+        // MARK: Tarjeta Label
+        view.addSubview(tarjetaLabel)
+        tarjetaLabel.frame = CGRect(x: 0, y: 0, width: 64, height: 21)
+        tarjetaLabel.backgroundColor = .white
+        tarjetaLabel.textColor = UIColor(red: 0.384, green: 0.384, blue: 0.384, alpha: 1)
+        tarjetaLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        tarjetaLabel.textAlignment = .center
+        tarjetaLabel.attributedText = NSMutableAttributedString(string: "TARJETA", attributes: [NSAttributedString.Key.kern: 0.7])
+        
+    
+        view.addSubview(tarjetaLabel)
+        tarjetaLabel.textAlignment = .center
+        tarjetaLabel.attributedText = NSMutableAttributedString(string: "TARJETA", attributes: [NSAttributedString.Key.kern: 0.7])
+
+        tarjetaLabel.translatesAutoresizingMaskIntoConstraints = false
+        tarjetaLabel.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        tarjetaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        tarjetaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
+        tarjetaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 115).isActive = true
+        
+        
+        // MARK: Componentes de tarjeta
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // MARK: Numero de tarjeta info
+        
+        
+
+        noTarjetaLabel.frame = CGRect(x: 0, y: 0, width: 98, height: 21)
+        noTarjetaLabel.backgroundColor = .white
+        noTarjetaLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        noTarjetaLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        noTarjetaLabel.text = "No. de Tarjeta"
+
+
+        view.addSubview(noTarjetaLabel)
+        noTarjetaLabel.translatesAutoresizingMaskIntoConstraints = false
+        noTarjetaLabel.widthAnchor.constraint(equalToConstant: 98).isActive = true
+        noTarjetaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        noTarjetaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
+        noTarjetaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 465).isActive = true
+
+    
+        noCuentaLabel.frame = CGRect(x: 0, y: 0, width: 149, height: 21)
+        noCuentaLabel.backgroundColor = .white
+        noCuentaLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        noCuentaLabel.font = UIFont(name: "Poppins-Regular", size: 14)
+        noCuentaLabel.text = "5439 2464 7664 2003"
+
+
+        view.addSubview(noCuentaLabel)
+        noCuentaLabel.translatesAutoresizingMaskIntoConstraints = false
+        noCuentaLabel.widthAnchor.constraint(equalToConstant: 149).isActive = true
+        noCuentaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        noCuentaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
+        noCuentaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 486).isActive = true
+        
+        
+        
+        // MARK: Vigencia info
+        
+    
+        vigenciaLabel.frame = CGRect(x: 0, y: 0, width: 63, height: 21)
+        vigenciaLabel.backgroundColor = .white
+        vigenciaLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        vigenciaLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        vigenciaLabel.text = "Vigencia"
+
+
+        view.addSubview(vigenciaLabel)
+        vigenciaLabel.translatesAutoresizingMaskIntoConstraints = false
+        vigenciaLabel.widthAnchor.constraint(equalToConstant: 63).isActive = true
+        vigenciaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        vigenciaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
+        vigenciaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 514).isActive = true
+        
+        
+        
+
+        fechaLabel.frame = CGRect(x: 0, y: 0, width: 33, height: 21)
+        fechaLabel.backgroundColor = .white
+        fechaLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        fechaLabel.font = UIFont(name: "Poppins-Regular", size: 14)
+        fechaLabel.text = "11/26"
+
+        
+        view.addSubview(fechaLabel)
+        fechaLabel.translatesAutoresizingMaskIntoConstraints = false
+        fechaLabel.widthAnchor.constraint(equalToConstant: 33).isActive = true
+        fechaLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        fechaLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
+        fechaLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 534).isActive = true
+        
+        //MARK: Datos del CVV
+        
+        
+        cvvLabel.frame = CGRect(x: 0, y: 0, width: 31, height: 21)
+        cvvLabel.backgroundColor = .white
+        cvvLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        cvvLabel.font = UIFont(name: "Poppins-Medium", size: 14)
+        cvvLabel.text = "CVV"
+
+
+        view.addSubview(cvvLabel)
+        cvvLabel.translatesAutoresizingMaskIntoConstraints = false
+        cvvLabel.widthAnchor.constraint(equalToConstant: 31).isActive = true
+        cvvLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        cvvLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 227).isActive = true
+        cvvLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 514).isActive = true
+        
+        
+        
+        cvvNumeroLabel.frame = CGRect(x: 0, y: 0, width: 22, height: 21)
+        cvvNumeroLabel.backgroundColor = .white
+        cvvNumeroLabel.textColor = UIColor(red: 0.302, green: 0.302, blue: 0.302, alpha: 1)
+        cvvNumeroLabel.font = UIFont(name: "Poppins-Regular", size: 14)
+        cvvNumeroLabel.text = "125"
+
+
+        
+
+        view.addSubview(cvvNumeroLabel)
+        cvvNumeroLabel.translatesAutoresizingMaskIntoConstraints = false
+        cvvNumeroLabel.widthAnchor.constraint(equalToConstant: 22).isActive = true
+        cvvNumeroLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
+        cvvNumeroLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 229).isActive = true
+        cvvNumeroLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 534).isActive = true
+        
+        
     }
+    
+    
+    
 
 }
